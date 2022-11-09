fdb0262473f3323dc54d9e38fea35a77a8b8f8f5
kiplimor <47780583+kiplimor@users.noreply.github.com>
2022-11-10 01:08:43 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Backend-Dev
+PHP form submission
+Please help with user authentication and database management using PHP and MySQL
+Can you also build the backend that allows form submission with PHP
+The Backend should be similar to this http://superessaywriters.com/index-2.php
+http://superessaywriters.com/index-2.php 
+Please use this site as a template for the backend functionalities.
---
+++ stellaressays.com/vendor/slider/home.js
@@ -0,0 +1,41 @@
+(function ($) {
+
+ "use strict";
+
+    
+
+		//---------------------------------------------
+
+		//Nivo slider
+
+		//---------------------------------------------
+
+
+
+			$('#ensign-nivoslider-3').nivoSlider({
+
+				effect: 'fade',
+
+				slices: 15,
+
+				boxCols: 8,
+
+				boxRows: 4,
+
+				animSpeed: 1000,
+
+				pauseTime: 7500,
+
+				startSlide: 0,
+
+				directionNav: true,
+
+				controlNavThumbs: true,
+
+				pauseOnHover: true,
+
+				manualAdvance: false
+
+			 }); 
+
+})(jQuery); 
